12266c4f7f1614212f21536450501a192997f218
Monique <moniquedgomes@outlook.com>
2024-07-08 22:00:47 -0300

    configuração de nova porta para a aplicação

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 const { sequelize, connectDB } = require('./services/dbConnect');
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001; 
 
 app.listen(PORT, () => {
     console.log(`the server is running on port ${PORT}`);
@@ -9,6 +9,7 @@ app.listen(PORT, () => {
 
 connectDB().then(async () => {
     console.log('Conectado ao banco de dados!');
+
     await sequelize.sync({ force: false });
 }).catch(err => {
     console.error('Erro ao conectar ao banco de dados:', err);
